6bae12b5aae709795613a3e2f90b67da61a3d10a
Peter Killick <peter.killick@metoffice.gov.uk>
2020-06-03 17:35:53 +0100

    Also update the writer public API name

--- nctotdb/__init__.py
+++ nctotdb/__init__.py
@@ -1,4 +1,4 @@
 from .data_model import NCDataModel
 from .grid_mappings import GridMapping, store_grid_mapping
 from .readers import TDBReader, ZarrReader
-from .writers import TDBWriter, ZarrWriter
+from .writers import TileDBWriter, ZarrWriter
